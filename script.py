9a6b5b0488a26e3cd4b0bbd680b70da168c33b60
unknown <Minskoleopc>
2024-04-25 21:22:40 +0530

    aded

---
+++ script.py
@@ -0,0 +1,61 @@
+x = 10
+print(x)
+x = 100
+print(x)
+
+# program 2
+
+q1 = 10 
+q2 = 5
+
+print(q1 + q2)
+print(q1 - q2)
+print(q1 * q2)
+print(q1 / q2)
+print(q1 % q2)
+
+s = 8
+t = 4
+
+print(s+t)
+print(s-t)
+print(s*t)
+print(s/t)
+print(s%t)
+
+# program 3
+# function 
+def Cal(x,y):
+    print(x+y)
+    print(x-y)
+    print(x*y)
+    print(x/y)
+    print(x%y)
+Cal(12,4)
+Cal(8,2)
+
+# program 4
+
+# function without parameter and without return 
+def add():
+    print(9+9)
+add()
+add()
+add()
+add()
+
+# function with parameter and without return type 
+def add2(x,y):
+    print(x+y)
+add2(8,4)
+add2(4,2)
+add2(9,3)
+
+# function with parameter and with return type 
+def add3(x,y):
+    return x + y
+x = add3(12,4)
+print(x)
+print(x + x)
+print(x/4)
+
---
+++ script2.py
@@ -0,0 +1,47 @@
+# type 
+
+# introvert      extrovert
+# calm              loud
+# less outing       more outing 
+# less social       more social
+
+# human --- type 
+# properties ---- color , gender , height , weight 
+# method -         walk() , talk()
+
+
+#vehicle -
+#properties - color , regNo , model
+#methods - start() , stop()
+
+#Bank acc 
+#properties - accNo , Type , Bal
+#methods - deposit() , withdrawl()
+
+
+x = 10 
+print(x)
+print(type(x))
+# 10 , -10 , 0
+
+y = 10.0
+print(y)
+print(type(y))
+# 12.5 , -12.5 , 0.0
+
+g = True
+print(g)
+print(type(g)) 
+# True False
+
+s = "chinmay"
+print(s)
+print(type(s))
+
+
+
+
+
+
+
+
